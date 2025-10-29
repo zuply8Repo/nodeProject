59b638b836118901c65ddfc957e0315442858e8d
Victor Solorzano <victor.solorzano08@gmail.com>
2025-10-29 23:17:58 +0100

    App running before auth implementation

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,8 +1,5 @@
 "use server";
 
-import { signIn } from "auth";
-import { AuthError } from "next-auth";
-
 import { z } from "zod";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
@@ -105,26 +102,6 @@ WHERE id = ${id}
 }
 
 export async function deleteInvoice(id: string) {
-  throw new Error("Failed to Delete Invoice");
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath("/dashboard/invoices");
-}
-
-export async function authenticate(
-  prevState: string | undefined,
-  formData: FormData
-) {
-  try {
-    await signIn("credentials", formData);
-  } catch (error) {
-    if (error instanceof AuthError) {
-      switch (error.type) {
-        case "CredentialsSignin":
-          return "Invalid credentials.";
-        default:
-          return "Something went wrong.";
-      }
-    }
-    throw error;
-  }
 }
